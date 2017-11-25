469de9a73d5be18f80ba89dce0e5dc04b6240f9a
Jeremy Tennant <tennantje@gmail.com>
2017-11-25 15:21:27 +1100

    Initial Commit

---
+++ google-cast-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  google-cast-swift
+//
+//  Created by jeremy on 25/11/17.
+//  Copyright © 2017 jeremy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
